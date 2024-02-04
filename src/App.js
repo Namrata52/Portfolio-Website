7c69db661c903feab3b4becd09593802b58b1c61
Namrata52 <namratalilaria29@gmail.com>
2024-02-04 16:35:29 +0530

    app js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import HeroSec from './components/HeroSec';
+
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      
+      <HeroSec></HeroSec>
+      <Skills></Skills>
+      <BrowserRouter>
+      <Projects></Projects>
+      </BrowserRouter>
+      
+      <Contact></Contact>
+      
+      
+      
     </div>
   );
 }
 
 export default App;
+
