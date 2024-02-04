7d913014d8557c5d04308f6486ea74914147f8a1
Namrata52 <namratalilaria29@gmail.com>
2024-02-04 16:58:09 +0530

    image list

---
+++ src/imageList.jsx
@@ -0,0 +1,59 @@
+import tailwind from './images/tailwind.png';
+import css from './images/CSS3_logo.png';
+import javascript from './images/JavaScript.png'
+import reactIcon from './images/React-icon.png';
+import html from './images/html.png';
+import java from './images/java-logo-1.png';
+import Bootstrap from './images/bootstrap-logo.png';
+import github from './images/github.png';
+import git from './images/Git.png'
+
+const imageList =[
+    {
+        id: 1,
+        src: html,
+        alt: "Image 1",
+      },
+      {
+        id: 2,
+        src: javascript,
+        alt: "Image 3",
+      },
+      {
+        id: 3,
+        src: css,
+        alt: "Image 2",
+      },
+      {
+        id: 4,
+        src: tailwind,
+        alt: "Image 4",
+      },
+      {
+        id: 5,
+        src: reactIcon,
+        alt: "Image 5",
+      },
+      {
+        id: 8,
+        src: git,
+        alt: "Image 8",
+      },
+      {
+        id: 9,
+        src: github,
+        alt: "Image 5",
+      },
+      
+      {
+        id: 7,
+        src: Bootstrap,
+        alt: "Image 7",
+      },
+      {
+        id: 6,
+        src: java,
+        alt: "Image 6",
+      },
+];
+export default imageList;
